bc7c63d023a56d207a33cb8b805190e2063b8178
Radhyy <raaakb87@gmail.com>
2026-06-03 14:13:59 +0700

    fix: make dashboards dynamic

--- app/(owner)/owner/dashboard/page.tsx
+++ app/(owner)/owner/dashboard/page.tsx
@@ -4,6 +4,8 @@ import TrenKeuanganChart from "@/components/charts/TrenKeuanganChart";
 import { format } from "date-fns";
 import { id } from "date-fns/locale";
 
+export const dynamic = "force-dynamic";
+
 export default async function OwnerDashboard() {
   // --- 1. Fetch Summary Data ---
   const [
